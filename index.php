f0600b761a133add0df7ffa65dfe74314ac5f37c
nuarita tri kartika <nuarita.kartika@gmail.com>
2022-10-24 11:08:20 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello World!<br>";
+
+$Nama = "Nuarita";
+$Usia = 21;
+
+echo "Nama saya <strong>$Nama</strong>. Saya berusia $Usia tahun.<br>";
+
+$NamaAdik = "Nua";
+$UsiaAdik = 12;
+
+echo "Nama adik saya <strong>$NamaAdik</strong>. Adik saya berusia $UsiaAdik tahun.<br>";
+
+$SelisihUsia = $Usia - $UsiaAdik;
+echo "Selisih usia saya dan usia adik saya adalah $SelisihUsia tahun.<br>"
+
+?>
