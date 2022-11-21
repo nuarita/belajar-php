c330212fc82d3ea1559258d27df41a8ac90c22ad
nuarita tri kartika <nuarita.kartika@gmail.com>
2022-11-21 13:00:18 +0700

    Penambahan index dan insert pada php

--- index.php
+++ index.php
@@ -1,18 +1,55 @@
 <?php
 
-echo "Hello World!<br>";
+// 1. Buat koneksi dengan mysql
+$con = mysqli_connect("localhost","root","","fakultas");
 
-$Nama = "Nuarita";
-$Usia = 21;
+// 2. Cek Koneksi dengan mysql
+if(mysqli_connect_errno()){
+    echo "Koneksi Gagal". mysqli_connect_error();
+}else{
+    echo "Koneksi Berhasil";
+}
 
-echo "Nama saya <strong>$Nama</strong>. Saya berusia $Usia tahun.<br>";
+//3. Membaca data dari table mysql
+$query = "SELECT * FROM mahasiswa";
 
-$NamaAdik = "Nua";
-$UsiaAdik = 12;
+//4. Menampilkan data, dengan menjalankan sql query
+$result = mysqli_query($con,$query);
+$mahasiswa = [];
+if ($result){
+    //menampilkan data satu per satu
+    while($row = mysqli_fetch_assoc($result)){
+        $mahasiswa[] = $row;
+    }
+    mysqli_free_result($result);
+}
 
-echo "Nama adik saya <strong>$NamaAdik</strong>. Adik saya berusia $UsiaAdik tahun.<br>";
-
-$SelisihUsia = $Usia - $UsiaAdik;
-echo "Selisih usia saya dan usia adik saya adalah $SelisihUsia tahun.<br>"
+//5. Menutup Koneksi mysql
+mysqli_close($con);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    <table border="1" style="width:100%;">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+        </tr>
+        <?php foreach($mahasiswa as $value): ?>
+        <tr>
+            <td><?php echo $value["nim"]; ?></td>
+            <td><?php echo $value["nama"]; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
